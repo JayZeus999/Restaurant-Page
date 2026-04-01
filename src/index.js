21850cfbc510524f3ba003845108a658e4d663d1
Jay Royce <faladejoseph400@gmail.com>
2026-04-01 23:46:14 +0100

    Set up contact.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./styles.css";
 import { loadHome } from "./home.js";
 import { loadMenu } from "./menu.js"
 import { loadAbout } from "./about.js"; 
+import { loadContact } from "./contact.js"; 
 
 const homeBtn = document.querySelector(".home-btn");
 homeBtn.addEventListener("click",() => {
@@ -19,4 +20,9 @@ menuBtn.addEventListener("click",() => {
 const aboutBtn = document.querySelector(".about-btn");
 aboutBtn.addEventListener("click",() => {
     loadAbout();
+});
+
+const contactBtn = document.querySelector(".contact-btn");
+contactBtn.addEventListener("click",() => {
+    loadContact();
 });
