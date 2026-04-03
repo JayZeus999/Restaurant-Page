b649a5ff452877b7495f0f9b986e3dfcb36c3190
Jay Royce <faladejoseph400@gmail.com>
2026-04-03 13:19:19 +0100

    Create common webpack config js file & refactor

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,38 +1,13 @@
 // webpack.dev.js
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
 
-export default {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
-};
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
+
+export default merge(common, {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+    hot: true,
+  },
+});
